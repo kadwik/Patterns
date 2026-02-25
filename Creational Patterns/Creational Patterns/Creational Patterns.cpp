04ca288bf7782f5c558f3d5f92d7ac358c0ad0c3
kadwik <vpehuy@gmail.com>
2026-02-25 19:48:26 +0500

    Сделал несколько приколов

--- Creational Patterns/Creational Patterns/Creational Patterns.cpp
+++ Creational Patterns/Creational Patterns/Creational Patterns.cpp
@@ -1,68 +1,96 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-class Character
-{
-public:
-	virtual std::string getType() = 0;
-	virtual void attack() = 0;
 
+class Character {
+public:
+    virtual string getType() = 0;
+    virtual string getName() { return getType(); }
+    virtual void attack() = 0;
+    virtual int getHealth() = 0;  
+    virtual ~Character() {}
 };
+
 class Warrior : public Character {
 public:
-	string getType() override { return "Воин"; }
-	void attack() override { cout << "воин атакует мечом!" << endl; }
-
+    string getType() override { return "Воин"; }
+    void attack() override { cout << "Воин атакует мечом!" << endl; }
+    int getHealth() override { return 100; }  
 };
+
 class Mage : public Character {
 public:
-	string getType() override { return "Маг"; }
-	void attack() override { cout << "Маг кастует перекрут яичек!" << endl; }
+    string getType() override { return "Мар"; }
+    void attack() override { cout << "Мар кастует огненный шар!" << endl; }
+    int getHealth() override { return 50; } 
 };
+
 class Archer : public Character {
 public:
-	string getType() override { return "Лучник"; }
-	void attack() override { cout << "Лучник стреляет из лука!" << endl; }
-};
-class CharacterFactory {
+    string getType() override { return "Лучник"; }
+    void attack() override { cout << "Лучник стреляет из лука!" << endl; }
+    int getHealth() override { return 70; }  
+
+
+class Knight : public Character {
 public:
-	virtual Character* createCharacter() = 0;
-	void spawnAndAttack() {
-		Character* chara = createCharacter();
-		cout << "Создан " << chara->getType() << ". ";
-		chara->attack();
-		delete chara;
-	}
-};
-class WarriorFactory : public CharacterFactory {
-public:
-	Character* createCharacter() override { return new Warrior(); }
-};
+    string getType() override {
+        return "Рыцарь";
+        void attack() override { cout << "Рыцарь атакует копьём!" << endl; }
+        int getHealth() override { return 80; }
+    };
 
-class MageFactory : public CharacterFactory {
-public:
-	Character* createCharacter() override { return new Mage(); }
-};
+    class CharacterFactory {
+    public:
+        virtual Character* createCharacter() = 0;
+        virtual ~CharacterFactory() {}
 
-class ArcherFactory : public CharacterFactory {
-public:
-	Character* createCharacter() override { return new Archer(); }
-};
+        void spawnAndAttack() {
+            Character* chara = createCharacter();
+            cout << "Создан " << chara->getName() << " с здоровьем " << chara->getHealth() << ". ";
+            chara->attack();
+            delete chara;
+        }
+    };
 
-int main() {
-	setlocale(0, "");
-	CharacterFactory* factory = new WarriorFactory();
-	factory->spawnAndAttack();
-	delete factory;
+    class WarriorFactory : public CharacterFactory {
+    public:
+        Character* createCharacter() override { return new Warrior(); }
+    };
 
-	factory = new MageFactory();
-	factory->spawnAndAttack();
-	delete factory;
+    class MageFactory : public CharacterFactory {
+    public:
+        Character* createCharacter() override { return new Mage(); }
+    };
 
-	factory = new ArcherFactory();
-	factory->spawnAndAttack();
-	
-	delete factory;
+    class ArcherFactory : public CharacterFactory {
+    public:
+        Character* createCharacter() override { return new Archer(); }
+    };
 
+    class KnightFactory : public CharacterFactory {
+    public:
+        Character* createCharacter() override { return new Knight(); }
+    };
 
-}
+    int main() {
+        setlocale(LC_ALL, "rus");
+
+        CharacterFactory* factory = new WarriorFactory();
+        factory->spawnAndAttack();
+        delete factory;
+
+        factory = new MageFactory();
+        factory->spawnAndAttack();
+        delete factory;
+
+        factory = new ArcherFactory();
+        factory->spawnAndAttack();
+        delete factory;
+
+        factory = new KnightFactory();
+        factory->spawnAndAttack();
+        delete factory;
+
+        return 0;
+    }
--- Structual Patterns/Structual Patterns/Structual Patterns.cpp
+++ Structual Patterns/Structual Patterns/Structual Patterns.cpp
@@ -1,15 +1,21 @@
-﻿#include <string>
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
+
 class Beverage {
 public:
-    virtual std::string getDescription() = 0;
+    virtual string getDescription() = 0;
     virtual double cost() = 0;
 };
-class Espresso : public Beverage {
+class Espresso : public Beverage { 
 public:
-    std::string getDescription() override {return "Эспрессо";}
-    double cost() override { return 1.99;}
+    string getDescription() override { return "Эспрессо"; }
+    double cost() override { return 1.99; }
+};
+class Tea : public Beverage {
+public:
+    string getDescription() override { return "Чай"; }
+    double cost() override { return 1.50; }
 };
 class CondimentDecorator : public Beverage {
 protected:
@@ -20,35 +26,45 @@ public:
     double cost() override { return beverage->cost(); }
 };
 class Milk : public CondimentDecorator {
-public: 
+public:
     Milk(Beverage* b) : CondimentDecorator(b) {}
-    std::string getDescription() override { return beverage->getDescription() + ", Молоко"; }
+    string getDescription() override { return beverage->getDescription() + ", Молоко"; }
     double cost() override { return beverage->cost() + 0.10; }
 };
 class Sugar : public CondimentDecorator {
 public:
     Sugar(Beverage* b) : CondimentDecorator(b) {}
-    std::string getDescription() override { return beverage->getDescription() + ", Сахар"; }
+    string getDescription() override { return beverage->getDescription() + ", Сахар"; }
     double cost() override { return beverage->cost() + 0.05; }
 };
 class Syrup : public CondimentDecorator {
 public:
     Syrup(Beverage* b) : CondimentDecorator(b) {}
-    std::string getDescription() override { return beverage->getDescription() + ", Сироп"; }
+    string getDescription() override { return beverage->getDescription() + ", Сироп"; }
     double cost() override { return beverage->cost() + 0.15; }
+};
+class Chocolate : public CondimentDecorator {
+public:
+    Chocolate(Beverage* b) : CondimentDecorator(b) {}
+    string getDescription() override { return beverage->getDescription() + ", Шоколад"; }
+    double cost() override { return beverage->cost() + 0.20; }
 };
 int main()
 {
-    Beverage* drink = new Espresso();
-    drink = new Milk(drink);
-    drink = new Sugar(drink);
-    drink = new Syrup(drink);
-   
-    cout << drink->getDescription() << " стоит$" << drink->cost() << endl;
-    Beverage* simpleDrink = new Espresso();
-    cout << simpleDrink->getDescription() << " стоит$" << simpleDrink->cost() << endl;
-
-    delete drink;
-    delete simpleDrink;
+    setlocale(LC_ALL, "rus");
+    Beverage* drink1 = new Espresso();
+    drink1 = new Milk(drink1);
+    drink1 = new Sugar(drink1);
+    drink1 = new Chocolate(drink1);
+    cout << drink1->getDescription() << " стоит $" << drink1->cost() << endl;
+    Beverage* drink2 = new Tea();
+    drink2 = new Milk(drink2);
+    drink2 = new Syrup(drink2);
+    cout << drink2->getDescription() << " стоит $" << drink2->cost() << endl;
+    Beverage* simpleTea = new Tea();
+    cout << simpleTea->getDescription() << " стоит $" << simpleTea->cost() << endl;
+    delete drink1;
+    delete drink2;
+    delete simpleTea;
+    return 0;
 }
-
